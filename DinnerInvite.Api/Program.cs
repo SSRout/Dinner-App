4c843b98146f430a12246e5964ab7116a95cdd5f
ssrout <smruti.sambit.rout@gmail.com>
2022-08-10 00:01:41 +0530

    Exception Handling added

--- DinnerInvite.Api/Program.cs
+++ DinnerInvite.Api/Program.cs
@@ -1,3 +1,5 @@
+using DinnerInvite.Api.Filters;
+using DinnerInvite.Api.Middleware;
 using DinnerInvite.Application;
 using DinnerInvite.Infrastructure;
 using Microsoft.AspNetCore.Builder;
@@ -6,11 +8,15 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+   // builder.Services.AddControllers(options=>options.Filters.Add<ErrorHandlingFilterAttribute>());//Using Error Filter
+   builder.Services.AddControllers();
 }
 
 var app=builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleWare>();//using Error MiddleWare
+    //using Global Error Exception Handler
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
