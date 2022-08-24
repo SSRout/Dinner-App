4f4d135753b2e2cf9e5f39936c3fa773c830a2a9
ssrout <smruti.sambit.rout@gmail.com>
2022-08-24 22:54:00 +0530

    validation behavior

--- DinnerInvite.Application/DependencyInjection.cs
+++ DinnerInvite.Application/DependencyInjection.cs
@@ -1,5 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
+using DinnerInvite.Application.Authentication.Commands.Register;
+using DinnerInvite.Application.Authentication.Common;
+using DinnerInvite.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using System.Reflection;
 
 namespace DinnerInvite.Application
 {
@@ -7,6 +13,8 @@ namespace DinnerInvite.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection service){
             service.AddMediatR(typeof(DependencyInjection).Assembly);
+            service.AddScoped(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return service;
         }
     }
