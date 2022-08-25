835dab7dd34cf5e34d0d512af49cb5cf9f3725ad
ssrout <smruti.sambit.rout@gmail.com>
2022-08-26 01:17:00 +0530

    jwt auth

---
+++ DinnerInvite.Api/Controllers/DinnersController.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DinnerInvite.Api.Controllers
+{
+    [Route("[controller]")]
+    public class DinnersController: ApiController
+    {
+        [HttpGet]
+        public IActionResult ListDinners(){
+            return Ok(Array.Empty<string>());
+        }
+    }
+}
